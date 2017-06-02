3e834c38600389e8424637ffadaf647aee39f8c5
caso224 <casocamara@hotmail.com>
2017-06-03 00:51:11 +0200

    implementation concrètes de la gestion des post avec les differents constructeurs

---
+++ Facebook/src/main/java/constructeurs/post/CreatePost.java
@@ -0,0 +1,55 @@
+package constructeurs.post;
+
+import generiqueI.post.IPost;
+
+/**
+ *  Constructeurs  CreatePost,
+ *
+ */
+public class CreatePost implements IPost {
+
+    private int id ;
+    private int like;
+    private int dislike;
+    private String contenu;
+
+    public CreatePost(int id, int like, int dislike, String contenu) {
+
+        assert(id >= 0) ;
+
+        this.id = id;
+        this.like = like;
+        this.dislike = dislike;
+        this.contenu = contenu;
+    }
+
+    @Override
+    public IPost changeContent(String content) {
+        return new CreatePost(id,like,dislike,content);
+    }
+
+    @Override
+    public IPost likeIt() {
+        return new CreatePost(id,like+1,dislike,contenu);
+    }
+
+    @Override
+    public IPost disLikeIt() {
+        return new CreatePost(id,like,dislike + 1, contenu);
+    }
+
+    @Override
+    public int getLike() {
+        return like;
+    }
+
+    @Override
+    public int getDislike() {
+        return dislike;
+    }
+
+    @Override
+    public int getIdPost() {
+        return id;
+    }
+}
---
+++ Facebook/src/main/java/constructeurs/post/DefaultPost.java
@@ -0,0 +1,48 @@
+package constructeurs.post;
+
+import generiqueI.post.IPost;
+
+/**
+ * Constructeur POST PAR DEFAUT
+ */
+public class DefaultPost implements IPost {
+
+   private int id;
+   private String contenu;
+
+    public DefaultPost(int id, String contenu) {
+        assert (id > 0 );
+        this.id = id;
+        this.contenu = contenu;
+    }
+
+    @Override
+    public IPost changeContent(String content) {
+        return new  DefaultPost(id,content);
+    }
+
+    @Override
+    public IPost likeIt()  {
+       throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IPost disLikeIt() {
+      throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getLike() {
+        return 0;
+    }
+
+    @Override
+    public int getDislike() {
+        return 0;
+    }
+
+    @Override
+    public int getIdPost() {
+        return 0;
+    }
+}
